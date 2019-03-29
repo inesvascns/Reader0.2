6c3d120faaedf5e140356ea05096c1c1c1bc6c80
inesvascns <48354182+inesvascns@users.noreply.github.com>
2019-03-29 00:51:38 +0000

    Criação botões "Pesquisar", "Adicionar Livro" e "Cancelar". Criação versão en e pt das srings.

--- app/src/main/java/pt/ipg/reader/Livros.java
+++ app/src/main/java/pt/ipg/reader/Livros.java
@@ -3,6 +3,9 @@ package pt.ipg.reader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class Livros extends AppCompatActivity {
@@ -12,7 +15,34 @@ public class Livros extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_livros);
 
-        Toast.makeText(Livros.this,"Livros",Toast.LENGTH_SHORT).show();
+        Button buttonCancelar = findViewById(R.id.buttonCancelar);
+        buttonCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
 
+        });
+
+    Button buttonPesquisar = findViewById(R.id.buttonPesquisar);
+
+    buttonPesquisar.setOnClickListener(new View.OnClickListener() {
+
+      }
+      }
+        @Override
+        public void onClick(View v) {
+            EditText editTextNome = findViewById(R.id.editText);
+            String mensagem = editTextNome.getText().toString();
+
+            if (mensagem.trim().length() == 0) {
+                editTextNome.setError(getString(R.string.campo_obrigatorio));
+            }else if (!mensagem.matches("[a-zA-Z ]+")){
+                editTextNome.setError(getString(R.string.char_especiais));
+            }else{
+                finish();
+                Toast.makeText(Livros.this,"Livro Pesquisado",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
-}
+
